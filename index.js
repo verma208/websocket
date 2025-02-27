f8145d2249c4150d26ea04e537f49625b3f92bcd
Pulkit Verma <verma208@umn.edu>
2025-02-27 15:41:33 -0600

    inital commit

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ wss.on('connection', (ws) => {
 });
 
 // Start the server
-const PORT = 3005;
+const PORT = process.env.PORT || 4000
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log(`WebSocket server is running on ws://localhost:${PORT}`);
